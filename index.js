4600b022ca664fd80634695334d58f34c2d674e2
Samuel Mondier <smondier@health.southalabama.edu>
2021-07-10 16:32:01 -0500

    fixed react build path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const cors     = require('cors');
+const path     = require('path')
 const getTemps = require('./input.js');
 
 // prepare express and allow for cors requests.
@@ -59,9 +60,9 @@ function mode(array) {
 
 // serve the compiled react app. intended for production. 
 if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname, "../frontend/build"));
+    app.use(express.static(path.join(__dirname, "./frontend/build")));
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+        res.sendFile(path.join(__dirname, './frontend/build/index.html'));
     });
 }
 
